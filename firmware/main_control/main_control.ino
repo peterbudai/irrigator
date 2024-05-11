814a5f34656cc929ba1acf0127e69f837bc5cc63
Péter Budai <buc@peterbudai.eu>
2024-05-12 00:52:50 +0200

    Start working on main controller logic

--- firmware/main_control/main_control.ino
+++ firmware/main_control/main_control.ino
@@ -1,4 +1,5 @@
 
+#define BLYNK_FIRMWARE_VERSION "0.1.0"
 #define BLYNK_NO_FANCY_LOGO
 // #define BLYNK_DEBUG
 #define BLYNK_PRINT Serial
@@ -16,47 +17,121 @@ const char ssid[] = BLYNK_WIFI_SSID;
 const char pass[] = BLYNK_WIFI_PASS;
 const char auth[] = BLYNK_AUTH_TOKEN;
 
-Adafruit_I2CDevice Relay(0x0A);
-Adafruit_I2CDevice User(0x0B);
+#define RELAY_1 0x01
+#define RELAY_2 0x02
+#define RELAY_OFF 0x10
+#define RELAY_ON 0x20
+
+class RelayController {
+private:
+  Adafruit_I2CDevice i2c;
+
+public:
+  RelayController()
+    : i2c(0x0A) {
+  }
+
+  void begin() {
+    if(i2c.begin()) {
+      Serial.print("[ OK ]");
+    } else {
+      Serial.print("[FAIL]");
+    }
+    Serial.println(" Relay controller init");
+  }
+
+  void setState(bool on) {
+    uint8_t cmd = (on ? RELAY_ON : RELAY_OFF) | RELAY_1 | RELAY_2;
+    if(i2c.write(&cmd, 1)) {
+      Serial.printf("[0x%02x]", cmd);
+    } else {
+      Serial.print("[FAIL]");
+    }
+      Serial.println(" Relay control");
+  }
+
+  void logState() {
+    uint8_t state = 0;
+    if(i2c.read(&state, 1)) {
+      Serial.printf("[0x%02x]", state);
+    } else {
+      Serial.print("[FAIL]");
+    }
+    Serial.println(" Relay status");
+  }
+};
+
+#define USER_LED_OFF 0x00
+#define USER_LED1_BLIP 0x01
+#define USER_LED1_BLINK 0x02
+#define USER_LED1_ON 0x03
+#define USER_LED2_BLIP 0x04
+#define USER_LED2_BLINK 0x08
+#define USER_LED2_ON 0x0C
+#define USER_SET_LED1 0x10
+#define USER_SET_LED2 0x20
+#define USER_ACK_BTN 0x80
+
+class UserController {
+private:
+  Adafruit_I2CDevice i2c;
+
+public:
+  UserController()
+    : i2c(0x0B) {
+  }
+
+  void begin() {
+    if(i2c.begin()) {
+      Serial.print("[ OK ]");
+    } else {
+      Serial.print("[FAIL]");
+    }
+    Serial.println(" User controller init");
+  }
+
+  void setState(uint8_t state) {
+    if(i2c.write(&state, 1)) {
+      Serial.printf("[0x%02x]", state);
+    } else {
+      Serial.print("[FAIL]");
+    }
+    Serial.println(" User control");
+  }
+
+  bool checkButton() {
+    uint8_t state = 0;
+    if(i2c.read(&state, 1)) {
+      Serial.printf("[0x%02x]");
+    } else {
+      Serial.print("[FAIL]");
+    }
+    Serial.println(" User status");
+    return (state & USER_ACK_BTN) != 0;
+  }
+};
+
+RelayController Relay;
+UserController User;
 RTC_DS3231 Clock;
 BlynkTimer Timer;
-
-void relaySet(bool on, uint8_t num) {
-  uint8_t cmd = (on ? 0x20 : 0x10) | (num & 0x03);
-  if(!Relay.write(&cmd, 1)) {
-    Serial.println("Communication error with relay");
-  }
-}
-
-void relayStatus() {
-  uint8_t status = 0;
-  Relay.read(&status, 1);
-  Serial.print("[0x");
-  Serial.print(status, 16);
-  Serial.println("] Relay status");
-}
-
-void userSet(bool ack, uint8_t leds) {
-  uint8_t cmd = (ack ? 0xB0 : 0x30) | (leds & 0x0F);
-  if(!User.write(&cmd, 1)) {
-    Serial.println("Communication error with user");
-  }
-}
-
-uint8_t userStatus() {
-  uint8_t status = 0;
-  User.read(&status, 1);
-  Serial.print("[0x");
-  Serial.print(status, 16);
-  Serial.println("] User status");
-  return status;
-}
 
 void checkConnection() {
   // State.setBlink((millis() % 1500) < 750);
   // State.setConnection(WiFi.status() == WL_CONNECTED ? (Blynk.connected() ? CONN_SERVER : CONN_WIFI) : CONN_NONE);
 }
 
+BLYNK_WRITE(InternalPinRTC) {     //check the value of InternalPinRTC  
+  DateTime now(param.asLong());   //store time in t variable
+  Serial.print("Current time: ");  
+  Serial.println(now.timestamp());
+  //Clock.adjust(now);
+}
+
+BLYNK_CONNECTED() {
+  Blynk.syncAll();
+  Blynk.sendInternal("rtc", "sync"); //request current local time for device
+}
 
 void setup() {
   Serial.begin(9600);
@@ -64,28 +139,21 @@ void setup() {
  
   // SDA on GPIO0, SCL on GPIO2
   Wire.begin(0, 2);
-  Serial.println("[ OK ] I2C");
+  Serial.println("[ OK ] I2C init");
 
-  if(User.begin()) {
-    Serial.print("[ OK ]");
+  User.begin();
+  User.setState(USER_ACK_BTN | USER_SET_LED1 | USER_SET_LED2 | USER_LED1_ON);
+  Relay.begin();
+
+  if(WiFi.begin(ssid, pass) == WL_CONNECT_FAILED) {
+    Serial.println("[FAIL] WiFi init");
   } else {
-    Serial.print("[FAIL]");
+    Serial.println("[ OK ] WiFi init");
   }
-  Serial.println(" User controller");
-
-  if(Relay.begin()) {
-    Serial.print("[ OK ]");
-  } else {
-    Serial.print("[FAIL]");
-  }
-  Serial.println(" Relay controller");
-
-
-  WiFi.begin(ssid, pass);
 
   Timer.setInterval(250L, checkConnection);
-  
   Blynk.config(auth);
+  Serial.println("[ OK ] Blynk init");
 
   delay(1000);
 }

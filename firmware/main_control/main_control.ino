05700336ffc5c905a0824598515c06bd46334849
Péter Budai <buc@peterbudai.eu>
2024-05-11 19:49:52 +0200

    Added RTC, Blynk and Wifi libraries to the main controller

--- firmware/main_control/main_control.ino
+++ firmware/main_control/main_control.ino
@@ -1,9 +1,25 @@
+
+#define BLYNK_NO_FANCY_LOGO
+// #define BLYNK_DEBUG
+#define BLYNK_PRINT Serial
+
+#include "secrets.h"      // Must be first, before any Blynk-related includes
+
 #include <Wire.h>
 #include <SoftwareSerial.h>
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
 #include <Adafruit_I2CDevice.h>
+#include <RTClib.h>
+
+const char ssid[] = BLYNK_WIFI_SSID;
+const char pass[] = BLYNK_WIFI_PASS;
+const char auth[] = BLYNK_AUTH_TOKEN;
 
 Adafruit_I2CDevice Relay(0x0A);
 Adafruit_I2CDevice User(0x0B);
+RTC_DS3231 Clock;
+BlynkTimer Timer;
 
 void relaySet(bool on, uint8_t num) {
   uint8_t cmd = (on ? 0x20 : 0x10) | (num & 0x03);
@@ -36,15 +52,26 @@ uint8_t userStatus() {
   return status;
 }
 
+void checkConnection() {
+  // State.setBlink((millis() % 1500) < 750);
+  // State.setConnection(WiFi.status() == WL_CONNECTED ? (Blynk.connected() ? CONN_SERVER : CONN_WIFI) : CONN_NONE);
+}
 
 
 void setup() {
   Serial.begin(9600);
   Serial.println("IrriGator");
  
-  // GPIO0(SDA), GPIO2(SCL)
+  // SDA on GPIO0, SCL on GPIO2
   Wire.begin(0, 2);
   Serial.println("[ OK ] I2C");
+
+  if(User.begin()) {
+    Serial.print("[ OK ]");
+  } else {
+    Serial.print("[FAIL]");
+  }
+  Serial.println(" User controller");
 
   if(Relay.begin()) {
     Serial.print("[ OK ]");
@@ -53,47 +80,17 @@ void setup() {
   }
   Serial.println(" Relay controller");
 
-  if(User.begin()) {
-    Serial.print("[ OK ]");
-  } else {
-    Serial.print("[FAIL]");
-  }
-  Serial.println(" User controller");
+
+  WiFi.begin(ssid, pass);
+
+  Timer.setInterval(250L, checkConnection);
+  
+  Blynk.config(auth);
+
+  delay(1000);
 }
 
 void loop() {
-  uint8_t us = 0;
-
-  delay(1000);
-  relayStatus();
-  relaySet(true, 1);
-  us = userStatus();
-  if(us & 0x80) {
-    us = (us + 1) & 0x0F;
-    userSet(true, us);
-  }
-  delay(1000);
-  relayStatus();
-  relaySet(true, 2);
-  us = userStatus();
-  if(us & 0x80) {
-    us = (us + 1) & 0x0F;
-    userSet(true, us);
-  }
-  delay(1000);
-  relayStatus();
-  relaySet(false, 1);
-  us = userStatus();
-  if(us & 0x80) {
-    us = (us + 1) & 0x0F;
-    userSet(true, us);
-  }
-  delay(1000);
-  relayStatus();
-  relaySet(false, 2);
-  us = userStatus();
-  if(us & 0x80) {
-    us = (us + 1) & 0x0F;
-    userSet(true, us);
-  }
+  Blynk.run();
+  Timer.run();
 }
---
+++ firmware/main_control/secrets.h
@@ -0,0 +1,18 @@
+/********************************************************************************
+
+Authentication secrets for WiFi and Blynk.
+
+These are example values. After checking out the repository, use the following
+Git command before changing the values in this file:
+
+    git update-index --assume-unchanged secrets.h
+
+This way you can prevent committing actual secrets to the remote repository.
+
+********************************************************************************/
+
+#define BLYNK_TEMPLATE_ID "TMPL123456789"
+#define BLYNK_TEMPLATE_NAME "IrriGator"
+#define BLYNK_AUTH_TOKEN "AaBbCcDdEeFf0123456789"
+#define BLYNK_WIFI_SSID "WifiRouterSSID"
+#define BLYNK_WIFI_PASS "Password12345"

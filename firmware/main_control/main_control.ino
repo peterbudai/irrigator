1eecdd26b378eb16d53f48356960eb668808e7cc
Péter Budai <buc@peterbudai.eu>
2024-05-13 00:24:13 +0200

    Added cloud syncing to clock properties

--- firmware/main_control/main_control.ino
+++ firmware/main_control/main_control.ino
@@ -76,6 +76,15 @@ class UserController {
 private:
   Adafruit_I2CDevice i2c;
 
+  void setState(uint8_t state) {
+    if(i2c.write(&state, 1)) {
+      Serial.printf("[0x%02x]", state);
+    } else {
+      Serial.print("[FAIL]");
+    }
+    Serial.println(" User control");
+  }
+
 public:
   UserController()
     : i2c(0x0B) {
@@ -88,15 +97,7 @@ public:
       Serial.print("[FAIL]");
     }
     Serial.println(" User controller init");
-  }
-
-  void setState(uint8_t state) {
-    if(i2c.write(&state, 1)) {
-      Serial.printf("[0x%02x]", state);
-    } else {
-      Serial.print("[FAIL]");
-    }
-    Serial.println(" User control");
+    this->setState(USER_ACK_BTN | USER_SET_LED1 | USER_SET_LED2 | USER_LED1_ON);
   }
 
   bool checkButton() {
@@ -109,28 +110,132 @@ public:
     Serial.println(" User status");
     return (state & USER_ACK_BTN) != 0;
   }
+
+  void setConnectionState(bool connected) {
+    if(connected) {
+      this->setState(USER_SET_LED1 | USER_LED1_BLIP);
+    } else {
+      this->setState(USER_SET_LED1 | USER_LED1_BLINK);
+    }
+  }
 };
+
+class ClockController {
+private:
+  bool enabled;
+  bool timeValid;
+  bool alarmValid[2];
+
+  RTC_DS3231 rtc;
+
+public:
+  ClockController()
+    : enabled(false), timeValid(false), alarmValid({false, false}) {
+  }
+
+  void begin() {
+    Serial.printf("[%s] Clock init\n", rtc.begin() ? " OK " : "FAIL");
+  }
+
+  void setTime(const DateTime& dt) {
+    rtc.adjust(dt);
+    timeValid = true;
+    Serial.printf("[ OK ] Current time set\n");  
+  }
+
+  void setAlarm(uint8_t alarm, const TimeInputParam& input) {
+    if(alarm < 1 || alarm > 2) {
+      return;
+    }
+
+    if(input.hasStartTime()) {
+      DateTime time(0, 0, 0, input.getStartHour(), input.getStartMinute(), input.getStartSecond());
+      bool success = false;
+      if(alarm == 1) {
+        success = rtc.setAlarm1(time, Ds3231Alarm1Mode::DS3231_A1_Hour);
+      }
+      if(alarm == 2) {
+        success = rtc.setAlarm2(time, Ds3231Alarm2Mode::DS3231_A2_Hour);
+      }
+      alarmValid[alarm - 1] = true;
+      Serial.printf("[%s] Alarm %d set\n", success ? " OK " : "FAIL", alarm);
+    } else {
+      rtc.disableAlarm(alarm);
+      alarmValid[alarm - 1] = false;
+      Serial.printf("[ OK ] Alarm %d disable\n", alarm);
+    }
+  }
+
+  float getTemperature() {
+    float temp = rtc.getTemperature();
+    Serial.printf("[ IN ] Temperature: %.2f C\n", temp);
+    return temp;
+  }
+};
+
+#define VirtualPinTimeOn V0
+#define VirtualPinTimeOff V1
+#define VirtualPinPinSchedule V2
+#define VirtualPinStatus V3
+#define VirtualPinTemperature V4
 
 RelayController Relay;
 UserController User;
-RTC_DS3231 Clock;
+ClockController Clock;
 BlynkTimer Timer;
 
-void checkConnection() {
-  // State.setBlink((millis() % 1500) < 750);
-  // State.setConnection(WiFi.status() == WL_CONNECTED ? (Blynk.connected() ? CONN_SERVER : CONN_WIFI) : CONN_NONE);
+// Handle receiving fresh real-time clock value from cloud
+BLYNK_WRITE(InternalPinRTC) {
+  // Convert Unix timestamp to date/time for RTC
+  DateTime time(param.asLong());
+  String str = time.timestamp();
+  Serial.printf("[RECV] Current time: %s\n", str.c_str());  
+  Clock.setTime(time);
 }
 
-BLYNK_WRITE(InternalPinRTC) {     //check the value of InternalPinRTC  
-  DateTime now(param.asLong());   //store time in t variable
-  Serial.print("Current time: ");  
-  Serial.println(now.timestamp());
-  //Clock.adjust(now);
+// Handle receiving new schedule ON time from cloud
+BLYNK_WRITE(VirtualPinTimeOn) {
+  TimeInputParam input(param);
+  Serial.printf("[RECV] On time: %02d:%02d\n", input.getStartHour(), input.getStartMinute());
+  Clock.setAlarm(1, input);
 }
 
+// Handle receiving new schedule OFF time from cloud
+BLYNK_WRITE(VirtualPinTimeOff) {
+  TimeInputParam input(param);
+  Serial.printf("[RECV] Off time: %02d:%02d\n", input.getStartHour(), input.getStartMinute());  
+  Clock.setAlarm(2, input);
+}
+
+// Handle successful connection to cloud server
 BLYNK_CONNECTED() {
-  Blynk.syncAll();
-  Blynk.sendInternal("rtc", "sync"); //request current local time for device
+  Serial.println("[ OK ] Cloud connected");
+
+  // Request up-to-date settings from cloud
+  Blynk.syncAll();                      // Schedule settings
+  Blynk.sendInternal("rtc", "sync");    // Current time
+
+  // Update user interface status LED
+  User.setConnectionState(true);
+}
+
+// Handle successful connection to cloud server
+BLYNK_DISCONNECTED() {
+  Serial.println("[FAIL] Cloud disconnected");
+
+  // Update user interface status LED
+  User.setConnectionState(false);
+}
+
+// Periodically check connection state
+void checkConnection() {
+  // Update user interface status LED
+  User.setConnectionState((WiFi.status() == WL_CONNECTED) && Blynk.connected());
+}
+
+void measureTemperature() {
+  Blynk.virtualWrite(VirtualPinTemperature, Clock.getTemperature()), 
+  Serial.println("[SEND] Temperature");
 }
 
 void setup() {
@@ -142,8 +247,8 @@ void setup() {
   Serial.println("[ OK ] I2C init");
 
   User.begin();
-  User.setState(USER_ACK_BTN | USER_SET_LED1 | USER_SET_LED2 | USER_LED1_ON);
   Relay.begin();
+  Clock.begin();
 
   if(WiFi.begin(ssid, pass) == WL_CONNECT_FAILED) {
     Serial.println("[FAIL] WiFi init");
@@ -151,11 +256,13 @@ void setup() {
     Serial.println("[ OK ] WiFi init");
   }
 
-  Timer.setInterval(250L, checkConnection);
+  //Timer.setInterval(1000L, checkConnection);
+  Timer.setInterval(60000L, measureTemperature);
   Blynk.config(auth);
-  Serial.println("[ OK ] Blynk init");
+  Serial.println("[ OK ] Cloud init");
 
   delay(1000);
+  User.setConnectionState(false);
 }
 
 void loop() {
